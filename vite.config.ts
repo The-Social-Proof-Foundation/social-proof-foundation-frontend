9874791404c3fa556e58f8790b4f6b114bdb6a58
Brandon Shaw <unobrandon@mac.com>
2024-12-12 01:48:27 -0600

    minor. favicon and port updates

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   server: {
-    port: parseInt(process.env.PORT || '3000'),
+    port: parseInt(process.env.PORT || '3001'),
     host: '0.0.0.0'
   },
   build: {
